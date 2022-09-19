d8e68b09270a3fe9e75941c31e6767ad3e580e4b
Najwa <najwa@iris.com.my>
2022-09-19 12:05:22 +0800

    first commit

---
+++ dbo.Table.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[ID] INT NOT NULL , 
+    [Name] VARCHAR(50) NULL, 
+    [Age] VARCHAR(50) NULL, 
+    CONSTRAINT [PK_Table] PRIMARY KEY ([ID])
+)
---
+++ member.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[ID] INT NOT NULL PRIMARY KEY, 
+    [Name] VARCHAR(50) NULL, 
+    [Age] VARCHAR(50) NULL
+)
